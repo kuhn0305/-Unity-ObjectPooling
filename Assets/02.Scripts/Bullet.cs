224db94971ec7b8f86b7e2f960e27850d504a3ca
HyunWoo Kang <kuhn0305@gmail.com>
2022-01-11 16:41:40 +0900

    Update Bullet.cs

--- Assets/02.Scripts/Bullet.cs
+++ Assets/02.Scripts/Bullet.cs
@@ -7,12 +7,16 @@ public class Bullet : MonoBehaviour
     private Vector3 direction; 
     public void Shoot(Vector3 direction) 
     { 
-        this.direction = direction; Invoke("DestroyBullet", 5f); 
+        this.direction = direction;
+        Invoke("DestroyBullet", 5f); 
     }
     
     public void DestroyBullet() 
     {
-        ObjectPool.ReturnObject(this); 
+        Destroy(gameObject);
+
+        /* Object Pooling */
+        //ObjectPool.ReturnObject(this); 
     } 
     
     void Update() 
