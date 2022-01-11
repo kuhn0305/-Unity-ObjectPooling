137ec3714fa0637b2b6948b8560cf532f85073ca
HyunWoo Kang <kuhn0305@gmail.com>
2022-01-11 16:41:36 +0900

    Update Shooter.cs

--- Assets/02.Scripts/Shooter.cs
+++ Assets/02.Scripts/Shooter.cs
@@ -17,7 +17,11 @@ public class Shooter : MonoBehaviour
         {
             RaycastHit hitResult; if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hitResult))
             {
-                var bullet = ObjectPool.GetObject(); // ¼öÁ¤
+                var bullet = Instantiate(bulletPrefab).GetComponent<Bullet>();
+                
+                /* Object Pooling */
+                //var bullet = ObjectPool.GetObject();
+
                 var direction = new Vector3(hitResult.point.x, transform.position.y, hitResult.point.z) - transform.position;
                 bullet.transform.position = direction.normalized;
                 bullet.Shoot(direction.normalized);
