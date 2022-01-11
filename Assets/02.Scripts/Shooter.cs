a602ea3901e885e6dcda9d80906cb7ad0baad8ec
HyunWoo Kang <kuhn0305@gmail.com>
2022-01-11 16:36:21 +0900

    Create Shooter.cs

---
+++ Assets/02.Scripts/Shooter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+    private Camera mainCam;
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hitResult; if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hitResult))
+            {
+                var bullet = ObjectPool.GetObject(); // ¼öÁ¤
+                var direction = new Vector3(hitResult.point.x, transform.position.y, hitResult.point.z) - transform.position;
+                bullet.transform.position = direction.normalized;
+                bullet.Shoot(direction.normalized);
+            }
+        }
+    }
+}
+
