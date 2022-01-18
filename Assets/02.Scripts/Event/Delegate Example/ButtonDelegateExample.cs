f04daf127032ee384d485fd0d542e7150ab0cca4
HyunWoo Kang <kuhn0305@gmail.com>
2022-01-18 16:45:22 +0900

    Update ButtonDelegateExample.cs

--- Assets/02.Scripts/Event/Delegate Example/ButtonDelegateExample.cs
+++ Assets/02.Scripts/Event/Delegate Example/ButtonDelegateExample.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
+
 
 public class ButtonDelegateExample : MonoBehaviour
 {
     public Button someButton;
 
+    private ColorPicker colorPicker;
+
+    public UnityEvent awakeEvent;
+
+    private void Awake()
+    {
+        awakeEvent?.Invoke();
+
+        colorPicker = GetComponent<ColorPicker>();
+    }
+
     void OnEnable()
     {
         //Register Button Events
-        someButton.onClick.AddListener(() => buttonCallBack("Hello World!", 88));
-        someButton.onClick.AddListener(delegate { buttonCallBack("Hi", 5733); });
+        someButton.onClick.AddListener(() => buttonCallBack(colorPicker.Color, colorPicker.Index));
+        someButton.onClick.AddListener(delegate 
+        { 
+            buttonCallBack("Hi", 5733); 
+        });
     }
 
     private void buttonCallBack(string myStringValue, int myIntValue)
