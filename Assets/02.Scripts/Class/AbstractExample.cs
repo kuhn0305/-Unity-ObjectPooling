c2dba2e9b9143a54b382dc15f1af937e5747e7b2
HyunWoo Kang <kuhn0305@gmail.com>
2022-01-20 17:51:29 +0900

    Update AbstractExample.cs

--- Assets/02.Scripts/Class/AbstractExample.cs
+++ Assets/02.Scripts/Class/AbstractExample.cs
@@ -4,13 +4,72 @@ using UnityEngine;
 
 public abstract class AbstractExample : MonoBehaviour
 {
-    public abstract void Speak();
+    protected abstract void Speak();
 }
 
-public class Dog : AbstractExample
+
+public class Dog : Animal
 {
-    public override void Speak()
+    public float size = 0.5f;
+
+    public override void Shouting()
+    {
+        Debug.Log("港港!");
+    }
+
+    protected override void Move()
     {
         Debug.Log("具克");
     }
+
+    protected override void Attack()
+    {
+        Debug.Log("具克");
+    }
+
+    public void Speak()
+    {
+
+    }
+}
+
+public class Cat : Animal
+{
+    float age = 0.5f;
+
+    public override void Shouting()
+    {
+        Debug.Log("具克");
+    }
+
+    protected override void Move()
+    {
+        Debug.Log("具克");
+    }
+
+    protected override void Attack()
+    {
+        Debug.Log("具克");
+    }
+}
+
+
+
+public abstract class Animal
+{
+    protected abstract void Move();
+    public abstract void Shouting();
+    protected abstract void Attack();
+
+    protected virtual void aaa()
+    {
+        Debug.Log("1234");
+    }
+}
+
+public abstract class Person
+{
+    protected abstract void Move();
+    public abstract void Shouting();
+    protected abstract void Attack();
 }
