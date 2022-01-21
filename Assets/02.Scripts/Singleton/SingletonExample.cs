34c742b5f1b5b58545e854dccf35ef6eadadc385
HyunWoo Kang <kuhn0305@gmail.com>
2022-01-21 16:48:30 +0900

    Create SingletonExample.cs

---
+++ Assets/02.Scripts/Singleton/SingletonExample.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonExample : MonoBehaviour
+{
+    private static SingletonExample instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(instance);
+        }
+        else        
+            instance = this;
+
+        // 모든 씬에서 유지
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public SingletonExample Instance
+    {
+        get { return instance; }
+    }
+
+    public static SingletonExample GetInstance()
+    {
+        if (instance == null)
+        {
+            Debug.LogError("SingletonExample 인스턴스가 존재하지 않습니다.");
+            return null;
+        }
+
+        return instance;
+    }
+}
