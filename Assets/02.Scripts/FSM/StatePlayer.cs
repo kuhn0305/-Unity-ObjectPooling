7e405277dec2e6de778ba7dd3e43bf5faf0c0ca7
HyunWoo Kang <kuhn0305@gmail.com>
2022-01-26 16:54:40 +0900

    Create StatePlayer.cs

---
+++ Assets/02.Scripts/FSM/StatePlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StatePlayer : MonoBehaviour
+{
+    public IdleState idleState = new IdleState();
+    public MoveState moveState = new MoveState();
+    public RunState runState = new RunState();
+
+    IState currentState;
+
+    protected virtual void Update()
+    {
+        currentState.Update();
+    }
+
+    public virtual void SetState(IState nextState)
+
+    {
+
+        // 다음 state로 이동구현
+        if (currentState != null)
+        {
+            // 기존의 상태가 존재했다면 OnExit()호출
+            currentState.OnExit();
+        }
+
+
+
+        // 다음state 시작
+        currentState = nextState;
+        currentState.OnEnter(this);
+    }
+}
